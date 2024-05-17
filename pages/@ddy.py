f36711c9f0fc9c1f7a56d8f761dcc3b0ecb8d930
gusgus374 <gusalvarez374@gmail.com>
2024-05-17 16:51:10 -0400

    added addy page

---
+++ pages/@ddy.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pathlib
+import streamlit as st
+import altair as alt
+#import folium
+#from folium.plugins import HeatMap
+#import seaborn as sns
+st.title("SLAY!")
+
+if st.button("Click for Taylor"):
+    st.image(image="./resources/taylor.png",width=600)
